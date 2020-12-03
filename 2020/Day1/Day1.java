b7fc318ba58ed9b02427a02da8e158a4bacb06d5
jsonballadares <jsonballadares@gmail.com>
2020-12-03 13:47:30 -0500

    added part2 for Day 1

--- 2020/Day1/Day1.java
+++ 2020/Day1/Day1.java
@@ -21,6 +21,12 @@ import java.util.Scanner;
  * to 2020; what do you get if you multiply them together?
  */
 
+ /**
+  * The Elves in accounting are thankful for your help; one of them even offers you a starfish coin they had left over from a past vacation. They offer you a second one if you can find three numbers in your expense report that meet the same criteria.
+  * Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them together produces the answer, 241861950.
+  * In your expense report, what is the product of the three entries that sum to 2020?
+  */
+
 public class Day1 {
     public static void main(String[] args) {
         List<Integer> arr = new ArrayList<Integer>(); 
@@ -34,15 +40,31 @@ public class Day1 {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        System.out.println("Answer is: " + solution(arr));
-
+        System.out.println("Part One answer is: " + solution(arr));
+        System.out.println("Part Two answer is: " + solution2(arr));
     }
     
+    //Part 1
     public static int solution(List<Integer> arr){
         for(int i = 0; i < arr.size(); i++){
             for(int j = 0; j < arr.size(); j++){
                 if(arr.get(i) + arr.get(j) == 2020){
                     return arr.get(i).intValue() * arr.get(j).intValue();
+                }
+            }
+        }
+        return -1;
+    }
+
+    //Part 2
+    public static int solution2(List<Integer> arr){
+        for(int i = 0; i < arr.size(); i++){
+            for(int j = 0; j < arr.size(); j++){
+                for(int k = 0; k < arr.size(); k++){
+                    if(arr.get(i) + arr.get(j) + arr.get(k) == 2020){
+                        //System.out.println(String.format("i: %s\nj: %s\nk: %s\n",arr.get(i),arr.get(j),arr.get(k)));
+                        return arr.get(i).intValue() * arr.get(j).intValue() * arr.get(k).intValue();
+                    }
                 }
             }
         }
